607767182269e53c7df79bcbea349a71bf7d56bd
Rania BOUTEKRABET <raniabtkt@gmail.com>
2021-11-22 17:27:14 +0100

    creation controller, models- installation pundit

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,2 +1,7 @@
 class PartiesController < ApplicationController
+
+  def create
+    @party = Party.new
+    authorize(@party)
+  end
 end
--- app/policies/party_policy.rb
+++ app/policies/party_policy.rb
@@ -1,7 +1,12 @@
 class PartyPolicy < ApplicationPolicy
   class Scope < Scope
+
     def resolve
       scope.all
     end
+    def create?
+      return true
+    end
+
   end
 end
