0ac2701904f85beebd466797aafef5ac85d4ae94
Albin Le Bon <lebon.albin@gmail.com>
2021-11-23 16:44:50 +0100

    adding fields to sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :parties
   has_many :meetings
   has_many :beverages, through: :meetings
   has_many :drinks, through: :beverages
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :sex, presence: true
