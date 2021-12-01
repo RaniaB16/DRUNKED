02df5a29c65fe905b47414136d50bd8f4c598a6c
Albin Le Bon <lebon.albin@gmail.com>
2021-12-01 09:03:47 +0100

    fix route to Friendship destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :parties
   resources :beverages
   resources :friendships, only: [:new, :destroy]
+  get "/friendships/:id", to: "friendships#destroy"
   get "/add_friends", to: "friendships#add_friends"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
